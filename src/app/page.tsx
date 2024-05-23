1c6c41ef6b0860d8eb617e1b1776cf1886878988
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-05-24 03:51:26 +0530

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="container mx-auto py-8">
+      <h1 className="text-4xl font-semibold text-center mb-8">Welcome to GrowthWell</h1>
+      <div className="flex justify-center mb-8">
+
+      </div>
+      <p className="text-lg text-gray-800 text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla felis eros, ullamcorper
+        nec condimentum id, placerat eget lacus. Mauris consectetur vestibulum justo, at lacinia
+        velit convallis eget. Nullam in arcu pulvinar, faucibus eros sed, dignissim nisi.
+      </p>
+      {/* Add more content as needed */}
+    </div>
+    </main>
+  );
+}
