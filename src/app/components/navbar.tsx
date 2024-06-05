a0b0ac34f5afd4f1b42fdae72969c873224812c4
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-06-05 21:03:46 +0530

    Update navbar.tsx

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,11 +4,13 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const navItems = [
   {
-    path: "/",
-    name: "Home",
+    path: "/shop",
+    name: "Shop",
   },
   {
     path: "/about",
@@ -18,6 +20,10 @@ const navItems = [
     path: "/contact",
     name: "Contact",
   },
+  {
+    path: "/cart",
+    name: <FontAwesomeIcon icon={faShoppingCart} className="text-gray-900 dark:text-white text-2xl cursor-pointer" />,
+  }
 ];
 
 const Navbar = () => {
@@ -26,10 +32,10 @@ const Navbar = () => {
   const [hoveredPath, setHoveredPath] = useState<string | null>(null);
 
   return (
-    <div className="bg-white dark:bg-neutral-900 shadow-md p-4 rounded-lg sticky top-0 z-50 backdrop-blur-sm">
+    <div className="bg-white dark:bg-neutral-900 shadow-md p-4 sticky top-0 z-50 backdrop-blur-sm">
       <nav className="flex items-center justify-between max-w-7xl mx-auto">
         <Link href="/">
-          <p className="text-2xl font-bold text-gray-900 dark:text-white">GrowthWell</p>
+          <p className="text-2xl font-bold text-gray-900 dark:text-white">Growthwell</p>
         </Link>
         <div className="flex space-x-8">
           {navItems.map((item) => {
@@ -40,14 +46,14 @@ const Navbar = () => {
                 <p
                   onMouseOver={() => setHoveredPath(item.path)}
                   onMouseLeave={() => setHoveredPath(null)}
-                  className={`relative px-3 py-2 rounded-md text-lg font-medium ${
+                  className={`relative px-3 py-2 rounded-full text-lg font-medium ${
                     isActive ? "text-gray-900 dark:text-white" : "text-gray-500 dark:text-gray-400"
                   } transition-colors duration-200`}
                 >
                   <span className="relative z-10">{item.name}</span>
                   {hoveredPath === item.path && (
                     <motion.div
-                      className="absolute inset-0 bg-gray-200 dark:bg-gray-800 rounded-md z-0"
+                      className="absolute inset-0 bg-gray-200 dark:bg-gray-800 rounded-full z-0"
                       layoutId="hoverBackground"
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
