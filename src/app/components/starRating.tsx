43d18929742167e4bf4d6a9d5b92298c404523bf
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-06-09 01:01:36 +0530

    Create starRating.tsx

---
+++ src/app/components/starRating.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export default function StarRating({ rating, setRating }) {
+  const [hover, setHover] = useState(0);
+
+  return (
+    <div className="flex">
+      {[...Array(5)].map((_, index) => {
+        index += 1;
+        return (
+          <button
+            type="button"
+            key={index}
+            className={index <= (hover || rating) ? 'text-yellow-500' : 'text-gray-400'}
+            onClick={() => setRating(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(rating)}
+          >
+            <span className="text-2xl">&#9733;</span>
+          </button>
+        );
+      })}
+    </div>
+  );
+}
