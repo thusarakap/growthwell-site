5616cea888bedaf44ab118021b77d63d980119bc
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-07-21 20:55:49 +0530

    Create navbarOffer.tsx

---
+++ src/app/components/navbarOffer.tsx
@@ -0,0 +1,12 @@
+import products from '../products.json'; 
+
+const NavbarOffer = () => {
+  
+    return (
+        <div className="w-full bg-blue-500 text-white text-center py-2 sticky top-20 z-50">
+        <p>Special offer: <b>30% OFF</b> on your first purchase!</p>
+      </div>
+    );
+  };
+  
+  export default NavbarOffer;
