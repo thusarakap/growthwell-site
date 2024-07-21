47f14e177e781af83de70831155ecf5f4e951e0a
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-07-21 20:55:27 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center text-white">
           Hello, About!
         </div>
       </main>
